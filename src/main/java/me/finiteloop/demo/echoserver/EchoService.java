bc57e72799694c64f134f744842122f7912c7cf8
Kunal Limaye <kunallimaye@gmail.com>
2015-10-06 13:48:52 +1100

    Added an EchoService

---
+++ src/main/java/me/finiteloop/demo/echoserver/EchoService.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package me.finiteloop.demo.echoserver;
+
+/**
+ * 
+ */
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+/**
+ * @author klimaye
+ *
+ */
+@Path("/echo")
+public class EchoService {
+
+	Properties properties = null;
+
+	public EchoService() throws IOException{
+	}
+	
+	
+	@GET
+	@Path("/{message}")
+	public Response echoMessage(@PathParam("message") String message) {
+
+		ResponseBuilder builder = null;
+		StringBuffer buffer = new StringBuffer("{");
+				buffer.append(message);
+				buffer.append("}");
+				buffer.append(" from Echo Service on OpenShift!");
+				
+		builder = Response.ok();
+		builder.entity(buffer.toString());
+
+		return builder.build();
+	}
+
+}
