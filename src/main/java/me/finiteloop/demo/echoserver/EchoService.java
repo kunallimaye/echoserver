4112b206207e45a0084662eaae9613fe98b63571
Kunal Limaye <kunallimaye@gmail.com>
2015-10-06 14:34:38 +1100

    Adding Produces and Consumes annotation

--- src/main/java/me/finiteloop/demo/echoserver/EchoService.java
+++ src/main/java/me/finiteloop/demo/echoserver/EchoService.java
@@ -10,9 +10,12 @@ package me.finiteloop.demo.echoserver;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
@@ -31,6 +34,8 @@ public class EchoService {
 	
 	@GET
 	@Path("/{message}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
 	public Response echoMessage(@PathParam("message") String message) {
 
 		ResponseBuilder builder = null;
