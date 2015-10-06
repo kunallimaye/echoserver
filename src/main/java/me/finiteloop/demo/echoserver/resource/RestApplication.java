e329956a49823364892d11296e8d91ff881fc2c8
Kunal Limaye <kunallimaye@gmail.com>
2015-10-06 13:50:38 +1100

    Added a RestApplication

---
+++ src/main/java/me/finiteloop/demo/echoserver/resource/RestApplication.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package me.finiteloop.demo.echoserver.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author klimaye
+ *
+ */
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
